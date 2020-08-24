45d64690a378756a2bb073e3012b11e8d1f48c85
Young Cho <badfriend87@hotmail.com>
2020-08-23 21:54:59 -0700

    Step 7 done with slight change to tailFound on getObjectStyleInCell

--- src/board.tsx
+++ src/board.tsx
@@ -1,14 +1,14 @@
-import React, {useState, useEffect, ClassAttributes, HTMLAttributes} from 'react';
-
+import React, {useState, useEffect} from 'react';
 
 interface Coordinates {
-  row: any;
-  col: any;
+  row: number;
+  col: number;
 }
 
 const Board = () => {
   const [grid, setGrid] = useState<Coordinates[][]>()
-  // const [snakeHead, setSnakeHead] = useState<Coordinates[{row: 5, col: 10}]>()
+  const [snakeHead, setSnakeHead] = useState<Coordinates>({row:5, col:10})
+  const [snakeTail, setSnakeTail] = useState<Coordinates[]>([{row:5, col:9},{row:5, col:8},{row:5, col:7}])
   useEffect(() => {initGrid()}, [])
     
   const initGrid = () => {
@@ -26,10 +26,30 @@ const Board = () => {
   const drawGrid = () => { 
       return grid && grid.map(
           (row)=> row.map((coords) => { 
-            return <div key={coords.row.toString() + '-' + coords.col.toString()} className='board-cell'> </div> 
-            //Todo: Figure out how to use this
-            //return <div key={${coords.row}-${coords.col}}> </div>
+            return (
+            <div 
+            key={`${coords.row}-${coords.col}`}
+              className={getObjectStyleInCell(coords.row, coords.col)}>                 
+            </div> 
+            )
       })) 
+  }
+
+  const getObjectStyleInCell = (row: number, col: number): string => {   
+    //Find Snake head location
+    if (snakeHead.row === row && snakeHead.col === col){
+      return 'snake-head'
+    }    
+
+    //Find snake tail location
+    //todo: make if (snakeTail.includes) work
+    for(let i = 0; i < snakeTail.length; i++){
+      if (snakeTail[i].row === row && snakeTail[i].col === col){
+        return 'snake-tail'
+      }
+    }  
+
+    return 'cell'
   }
 
   return <div className='board-background'>{grid && drawGrid()}</div>
