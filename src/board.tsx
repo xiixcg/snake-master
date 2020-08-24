f0673a79785399afc6e0dc6da175d659c9f826a1
Young Cho <badfriend87@hotmail.com>
2020-08-23 19:02:02 -0700

    step 5 done with improvise on gridDraw

--- src/board.tsx
+++ src/board.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect, ClassAttributes, HTMLAttributes} from 'react';
 
-const Board: React.FC = () => {
-  return (
-    <div className='board-background'>
-      Board
-    </div>
-  );
+
+interface Coordinates {
+  row: any;
+  col: any;
+}
+
+const Board = () => {
+  const [grid, setGrid] = useState<Coordinates[][]>()
+  // const [snakeHead, setSnakeHead] = useState<Coordinates[{row: 5, col: 10}]>()
+  useEffect(() => {initGrid()}, [])
+    
+  const initGrid = () => {
+    const grid = []; 
+    for (let row = 0; row < 50; row++) { 
+      const cols = []; 
+      for (let col = 0; col < 50; col++) { 
+        cols.push({row, col}); 
+      } 
+      grid.push(cols); 
+    } 
+    setGrid(grid); 
+  }
+  
+  const drawGrid = () => { 
+      return grid && grid.map(
+          (row)=> row.map((coords) => { 
+            return <div key={coords.row.toString() + '-' + coords.col.toString()} className='board-cell'> </div> 
+            //Todo: Figure out how to use this
+            //return <div key={${coords.row}-${coords.col}}> </div>
+      })) 
+  }
+
+  return <div className='board-background'>{grid && drawGrid()}</div>
 }
 
 export default Board;
+
