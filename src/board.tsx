da308a9e03eddbe7f234352e1809f00a624ffbf0
Young Cho <badfriend87@hotmail.com>
2020-08-26 20:30:59 -0700

    Features added: 1.Preventing direction inversion 2.Pause retaining velocity 3.Pause not enabled on game starting 4.Pause not retained after reset 5.Snake movement speed up using tickInterval 6.Snake direction don't change when Paused
    Features needed:
    1.Winning situation
    2.Setting page to control snake speed and board size

--- src/board.tsx
+++ src/board.tsx
@@ -1,21 +1,20 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react'
 
 interface Coordinates {
-  row: number;
-  col: number;
+  row: number
+  col: number
 }
 
 interface Velocity{
-  rowV: number;
-  colV: number;
+  rowV: number
+  colV: number
 }
 
-enum snakeDirection{
-  left = -1,
-  right = 1,
-  up = -1,
-  down = 1,
-  stop = 0
+enum Direction{
+  left,
+  right,
+  up,
+  down
 }
 
 let gameWidth = 50
@@ -24,76 +23,89 @@ let gameHeight = 50
 export const Board = () => {
   //set react state
   const [grid, setGrid] = useState<Coordinates[][]>()
-  const [snakeHead, setSnakeHead] = useState<Coordinates>({row:5, col:10})
+  const [snakeHead, setSnakeHead] = useState<Coordinates>({row:5, col: 10})
   const [snakeTail, setSnakeTail] = useState<Coordinates[]>([{row:5, col:9},{row:5, col:8},{row:5, col:7}])
   const [food, setFood] = useState<Coordinates>({row:-1, col:-1})
 
   const[tick, setTick] = useState<number>(0)
   const[snakeVelocity, setSnakeVelocity] = useState<Velocity>({rowV: 0, colV: 0})
-  const[direction, setDirection] = useState<snakeDirection>(snakeDirection.right)
   const[lost, setLost] = useState<boolean>(false)
   const[pause, setPause] = useState<boolean>(false)
   const[reset, setReset] = useState<boolean>(false)
   const[score, setScore] = useState<number>(0)  
+  const[snakeDirection, setSnakeDirection] = useState<Direction>(Direction.right)
+  const[tickInterval, setTickInterval] = useState<number>(300)  
+  const[pauseSnakeVelocity, setPauseSnakeVelocity] = useState<Velocity>({rowV: 0, colV: 0})
 
   useEffect(() => {
     if(reset){
       resetAll()
     }
     initGrid()
-    placeFood()   
-
-    const timer = window.setInterval(() => {
-      setTick(prevState => prevState + 1)
-    }, 250);
-
-    document.addEventListener('keydown', onKeyDown); 
-      
-    return () => { 
-      document.removeEventListener('keydown', onKeyDown); 
-      window.clearInterval(timer); 
-    }
+    placeFood()      
   }, [reset])  // eslint-disable-line react-hooks/exhaustive-deps
   
-  useEffect(() => {
+  useEffect(() => {    
     gameLogic()
+    console.log(snakeDirection)
     if (!pause){
       moveSnake()
     }
+
+    document.addEventListener('keydown', onKeyDown)
+    const timer = window.setInterval(() => {
+      setTick(prevState => prevState + 1)
+    }, tickInterval)
+    
+    return () => { 
+      document.removeEventListener('keydown', onKeyDown)
+      window.clearInterval(timer) 
+    }
   }, [tick])  // eslint-disable-line react-hooks/exhaustive-deps
 
+  useEffect(() => {
+    if(tickInterval >= 100){
+      setTickInterval(tickInterval - 50)
+    }
+  }, [food])  // eslint-disable-line react-hooks/exhaustive-deps
+
   const resetAll = () =>{
-    setSnakeHead({row: 5, col: 10}) 
+    setSnakeHead({row:5, col: 10}) 
     setSnakeTail([{row: 5, col: 9}, {row: 5, col: 8}, {row: 5, col: 7}]) 
-    setLost(false) 
-    setSnakeVelocity({rowV: 0, colV: 0}) 
     setTick(0)
+    setSnakeVelocity({rowV: 0, colV: 0}) 
+    setLost(false) 
+    setPause(false)
     setReset(false)
     setScore(0)
+    setSnakeDirection(Direction.right)
+    setTickInterval(300)
+    setPauseSnakeVelocity({rowV:0, colV: 0})
   }
     
+//#region Board Setup
   const initGrid = () => {
-    const grid = []; 
+    const grid = []
     for (let row = 0; row < gameHeight; row++) { 
-      const cols = []; 
+      const cols = []
       for (let col = 0; col < gameWidth; col++) { 
-        cols.push({row, col}); 
+        cols.push({row, col})
       } 
-      grid.push(cols); 
+      grid.push(cols)
     } 
-    setGrid(grid); 
+    setGrid(grid)
   }
 
   const drawGrid = () => { 
-      return grid && grid.map(
-          (row)=> row.map((coords) => { 
-            return (
-            <div 
-            key={`${coords.row}-${coords.col}`}
-              className={getObjectStyleInCell(coords.row, coords.col)}>                 
-            </div> 
-            )
-      })) 
+    return grid && grid.map(
+        (row)=> row.map((coords) => { 
+          return (
+          <div 
+          key={`${coords.row}-${coords.col}`}
+            className={getObjectStyleInCell(coords.row, coords.col)}>                 
+          </div> 
+          )
+    })) 
   }
 
   const getObjectStyleInCell = (row: number, col: number): string => {   
@@ -123,45 +135,68 @@ export const Board = () => {
   const placeFood = () => {
     setFood({row: Math.floor(Math.random() * gameWidth), col: Math.floor(Math.random() * gameHeight)}) 
   }  
+//#endregion 
 
+//#region Snake Movement
   const onKeyDown = (event: KeyboardEvent) => {
-    
-    console.log(pause)
-    if (pause){
+    if(pause){
       return
     }
     //Handle key press when snake is not paused
     switch(event.keyCode){
-        case 37:
-        case 100:
-          //left: do not invert direction and go right to left
-          if(snakeVelocity.colV !== 1){
-            setSnakeVelocity({rowV:0, colV: -1})
-          }
-          break
-        case 38:
-        case 104:
-          //up: do not invert direction and go down to up
-          if(snakeVelocity.rowV !== 1){
-            setSnakeVelocity({rowV: -1, colV: 0})
-          }
-          break
-        case 39:
-        case 102:
-          //right: do not invert direction and go left to right
-          if(snakeVelocity.colV !== -1){
-            setSnakeVelocity({rowV: 0, colV: 1})
-          }
-          break
-        case 40:
-        case 98:
-          //down: do not invert direction and go up from down
-          if(snakeVelocity.rowV !== -1){
-            setSnakeVelocity({rowV: 1, colV: 0})
-          }
-          break
+      case 37:
+      case 100:
+        //left: do not invert direction and go right to left
+        if(snakeDirection !== Direction.right){
+          setSnakeVelocity({rowV:0, colV: -1})
+          setSnakeDirection(Direction.left)
+        }          
+        break
+      case 38:
+      case 104:
+        //up: do not invert direction and go down to up
+        if(snakeDirection !== Direction.down){
+          setSnakeVelocity({rowV: -1, colV: 0})
+          setSnakeDirection(Direction.up)
+        }
+        break
+      case 39:
+      case 102:
+        //right: do not invert direction and go left to right
+        if(snakeDirection !== Direction.left){
+          setSnakeVelocity({rowV: 0, colV: 1})
+          setSnakeDirection(Direction.right)
+        }
+        break
+      case 40:
+      case 98:
+        //down: do not invert direction and go up from down
+        if(snakeDirection !== Direction.up){
+          setSnakeVelocity({rowV: 1, colV: 0})
+          setSnakeDirection(Direction.down)
+        }
+        break
     }
-    console.log(snakeVelocity)
+  }
+
+  const isOnFirstTail = (i: number): boolean => {
+    return i === 0
+  }
+
+  const isOnLastTail = (i: number): boolean => {
+    return i === snakeTail.length -1
+  }
+
+  const isFoodEaten = (): boolean => {
+    return snakeHead.row === food.row && snakeHead.col === food.col
+  }  
+
+  const isWallHit = () => {
+    return snakeHead.row < 0 || snakeHead.col < 0 || snakeHead.row > gameHeight || snakeHead.col > gameWidth
+  }
+
+  const isTailHit = () => {
+    return snakeTail.find((tail) => {return isObjectInCell(tail, snakeHead.row, snakeHead.col)})
   }
 
   const moveSnake = () => {
@@ -169,7 +204,7 @@ export const Board = () => {
       //Move the tail following the head
       let newTail: Coordinates[] = []
       for (let i = 0; i < snakeTail.length; i++){
-        if (isOnFirstTail(i)){
+        if (isOnFirstTail(i)){  //First tail need to add snakeHead first
           newTail.push({row:snakeHead.row, col:snakeHead.col})
           newTail.push(snakeTail[i])
         }
@@ -192,18 +227,6 @@ export const Board = () => {
     }
   }
 
-  const isOnFirstTail = (i: number): boolean => {
-    return i === 0
-  }
-
-  const isOnLastTail = (i: number): boolean => {
-    return i === snakeTail.length -1
-  }
-
-  const isFoodEaten = (): boolean => {
-    return snakeHead.row === food.row && snakeHead.col === food.col
-  }  
-
   const gameLogic = () => {
     if (isFoodEaten()){     
       placeFood()
@@ -221,15 +244,9 @@ export const Board = () => {
       setSnakeVelocity({rowV: 0, colV: 0})
     }
   }
+//#endregion
 
-  const isWallHit = () => {
-    return snakeHead.row < 0 || snakeHead.col < 0 || snakeHead.row > gameHeight || snakeHead.col > gameWidth
-  }
-
-  const isTailHit = () => {
-    return snakeTail.find((tail) => {return isObjectInCell(tail, snakeHead.row, snakeHead.col)})
-  }
-
+//#region  Board Display
   const drawSnakeBoard = () => {
     return (
       <div className='snake-board-background'>
@@ -248,10 +265,24 @@ export const Board = () => {
     )
   }
 
+  //Pause will retain the snake velocity. Unpause will continue snake's path as before pause
+  const pauseClicked = () => {
+    if(snakeVelocity.rowV === 0 && snakeVelocity.colV === 0){
+      if(!pause){
+        return  //This is game start. Shouldn't enable pause here
+      } 
+      setSnakeVelocity({rowV: pauseSnakeVelocity.rowV, colV: pauseSnakeVelocity.colV})
+    }
+    else{
+      setPauseSnakeVelocity({rowV: snakeVelocity.rowV, colV: snakeVelocity.colV})
+    }
+    setPause(!pause)
+  }
+
   const drawButtons = () => {
     return (
       <div className='button-board'>
-        <button className='button' onClick={() => setPause(!pause) }>
+        <button className='button' onClick={() => pauseClicked() }>
           Pause
         </button>
         <button className='button' onClick={() => setReset(true)}>
@@ -260,6 +291,7 @@ export const Board = () => {
       </div>
     )
   }
+//#endregion
  
   return (    
     <div className='game-board'>
@@ -269,7 +301,5 @@ export const Board = () => {
     </div>
   )  
 }
-
-
-export default Board;
+export default Board
 
